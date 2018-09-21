90cc3396463ca35f542a5d23bb585903409aea08
ik013043z1 <ik013043z1@H013043.NET>
2018-09-21 10:26:13 +0200

    Proba

---
+++ DataTypes.java
@@ -0,0 +1,22 @@
+
+public class DataTypes {
+public static void main(String args[])
+{
+	//character
+	char a = 'C';
+	//numbers
+	int i=999999999;
+	//short numbers
+	byte b = 99;
+	//decimals
+	float f = 9.516351f;
+	//long
+	long l = 999999999;
+	
+	System.out.println("char: " + a);
+	System.out.println("int: " + i);
+	System.out.println("byte: " + b);
+	System.out.println("float: " + f);
+	System.out.println("long: " + l);
+}
+}
